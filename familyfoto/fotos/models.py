6f6d3f6f37b2fe43720ddb3069c933c0cfc71f91
jakubkotlorz <jkotlorz@protonmail.com>
2023-02-19 14:33:19 +0100

    New model: Foto, to contain basic picture info

--- familyfoto/fotos/models.py
+++ familyfoto/fotos/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Foto(models.Model):
+    """Keeps information about uploaded media file."""
+    title = models.CharField(max_length=200, blank=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='fotos/')
+    added = models.DateField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.title if self.title else self.image.url
+
+    def get_image_url(self):
+        return f"{settings.MEDIA_URL}/{self.image}"
--- familyfoto/fotos/views.py
+++ familyfoto/fotos/views.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.shortcuts import render
+from .models import Foto
 
 def index(request):
     images = [f"{settings.MEDIA_URL}fotos/img-3.jpg",
             f"{settings.MEDIA_URL}fotos/img-2.jpg", 
             f"{settings.MEDIA_URL}fotos/img-1.jpg"]
+    images = Foto.objects.all()
     context = {'images': images}
     return render(request, 'fotos/index.html', context)
