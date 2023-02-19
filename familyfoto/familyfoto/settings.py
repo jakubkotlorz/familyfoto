32677dac6b71a37b88116ee0dbcc1812eefd658d
jakubkotlorz <jkotlorz@protonmail.com>
2023-02-19 11:35:20 +0100

    Handling media files in development added

--- familyfoto/familyfoto/settings.py
+++ familyfoto/familyfoto/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fotos.apps.FotosConfig'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public', 'media')
--- familyfoto/fotos/views.py
+++ familyfoto/fotos/views.py
@@ -1,4 +1,9 @@
-from django.http import HttpResponse
+from django.conf import settings
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    images = [f"{settings.MEDIA_URL}fotos/img-3.jpg",
+            f"{settings.MEDIA_URL}fotos/img-2.jpg", 
+            f"{settings.MEDIA_URL}fotos/img-1.jpg"]
+    context = {'images': images}
+    return render(request, 'fotos/index.html', context)
